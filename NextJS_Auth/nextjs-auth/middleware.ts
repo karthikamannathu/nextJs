4489894c31b41e44c2c6cf5a064efd4e1f16cfea
karthika krishnankutty <karthikamannathu@gmail.com>
2024-05-30 16:02:32 +0530

    next auth complitted

---
+++ NextJS_Auth/nextjs-auth/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server"
+
+
+const isAuthenticated = true;
+
+export function middleware(request:NextRequest){
+
+if(isAuthenticated){
+    if(["/login"].includes(request.nextUrl.pathname))
+  return  NextResponse.redirect(new URL ("/dashboared",request.url))
+}
+
+
+if(isAuthenticated!){
+    return NextResponse.next()
+}
+
+if(!isAuthenticated){
+    if(["dashboared","/profile"].includes(request.nextUrl.pathname))
+        return NextResponse.redirect(new URL("/login",request.url))
+}
+
+
+}
